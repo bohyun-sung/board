fa97c3a8390d174fda3d2d5293127271f0c8a1ac
성보현 <djajsl8799@naver.com>
2026-03-12 06:54:05 +0900

    배포용 http 문제 및 favicon.ico 무시 처리

--- src/main/java/com/toyproject/board/api/security/config/SecurityConfig.java
+++ src/main/java/com/toyproject/board/api/security/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -87,5 +88,11 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/favicon.ico", "/error");
     }
 }
