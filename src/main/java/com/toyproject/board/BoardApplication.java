ff240fbc0de190d20ae2fea4aa23fceb57c4d8df
성보현 <djajsl8799@naver.com>
2026-03-16 04:20:44 +0900

    spring server image 분리-4 global-bundle.pem > trustServerCertificate=true 추가 - /etc/localtime:/etc/localtime:ro 추가

--- src/main/java/com/toyproject/board/BoardApplication.java
+++ src/main/java/com/toyproject/board/BoardApplication.java
@@ -1,13 +1,20 @@
 package com.toyproject.board;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @SpringBootApplication
 public class BoardApplication {
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
